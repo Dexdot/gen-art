d063adbb9d4bd3faeb3b2e22028b783336dd03c4
Dexdot <dexdotapb@gmail.com>
2018-12-22 01:16:32 +0300

    rotation, text, save & restore, get seed

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ const settings = {
   dimensions: [2048, 2048]
 };
 
+random.setSeed(random.getRandomSeed());
+console.log(random.getRandomSeed());
+
 const sketch = () => {
   const colorCount = random.rangeFloor(1, 6);
   const palette = random.shuffle(random.pick(palettes)).slice(0, colorCount);
@@ -18,10 +21,13 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
+        const radius = Math.abs(random.noise2D(u, v)) * 0.115;
+
         points.push({
           color: random.pick(palette),
-          radius: Math.abs(0.01 + random.gaussian() * 0.01),
-          position: [u, v]
+          position: [u, v],
+          rotation: random.noise2D(u, v) * 2,
+          radius
         });
       }
     }
@@ -29,10 +35,7 @@ const sketch = () => {
     return points;
   };
 
-  random.setSeed(
-    `какой-то уникальный набор рандома, дающий всегда один результат`
-  );
-  const points = createGrid(30).filter(() => random.value() > 0.5);
+  const points = createGrid(40).filter(() => random.value() > 0.5);
   const margin = 400;
 
   return ({ context, width, height }) => {
@@ -40,17 +43,25 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     points.forEach(data => {
-      const { position, radius, color } = data;
+      const { position, radius, color, rotation } = data;
 
       const [u, v] = position;
 
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      context.beginPath();
-      context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.beginPath();
+      // context.arc(x, y, radius * width, 0, Math.PI * 2, false);
+      // context.fillStyle = color;
+      // context.fill();
+
+      context.save();
       context.fillStyle = color;
-      context.fill();
+      context.font = `${radius * width}px "Gilroy"`;
+      context.translate(x, y);
+      context.rotate(rotation);
+      context.fillText('-', 0, 0);
+      context.restore();
     });
   };
 };
